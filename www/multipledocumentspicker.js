7544aadb4be0cde55ec590e0ab26438f1ae3b117
Naveen Agarwal <agarwalnaveen22@gmail.com>
2020-10-05 15:15:46 +0530

    updated plugin description and calling pattern

--- www/multipledocumentspicker.js
+++ www/multipledocumentspicker.js
@@ -1,19 +1,11 @@
-// Empty constructor
+var exec = require('cordova/exec');
+
 function MultipleDocumentsPicker() {}
 
-// The function that passes work along to native shells
 MultipleDocumentsPicker.prototype.pick = function(type, successCallback, errorCallback) {
   var options = {};
   options.type = type;
-  cordova.exec(successCallback, errorCallback, 'MultipleDocumentsPicker', 'pick', [options]);
+  exec(successCallback, errorCallback, 'MultipleDocumentsPicker', 'pick', [options]);
 }
 
-// Installation constructor that binds MultipleDocumentsPicker to window
-MultipleDocumentsPicker.install = function() {
-  if (!window.plugins) {
-    window.plugins = {};
-  }
-  window.plugins.multipleDocumentsPicker = new MultipleDocumentsPicker();
-  return window.plugins.multipleDocumentsPicker;
-};
-cordova.addConstructor(MultipleDocumentsPicker.install);
+module.exports = new MultipleDocumentsPicker();
